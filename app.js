95101e041039a3e87d4d2be86611864ddf06e580
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-02-20 13:08:41 +0530

    dispalyed public stories

--- app.js
+++ app.js
@@ -28,12 +28,16 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Handlebars Helpers
-const { formatDate } = require('./helpers/hbs');
+const { formatDate, stripTags, truncate, editIcon } = require('./helpers/hbs');
 
 //Handlebars
 app.engine(
   '.hbs',
-  exphbs({ helpers: { formatDate }, defaultLayout: 'main', extname: '.hbs' })
+  exphbs({
+    helpers: { formatDate, stripTags, truncate, editIcon },
+    defaultLayout: 'main',
+    extname: '.hbs',
+  })
 );
 app.set('view engine', '.hbs');
 
@@ -50,6 +54,12 @@ app.use(
 // Passport Middleware
 app.use(passport.initialize());
 app.use(passport.session());
+
+//Set Global Variable
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null;
+  next();
+});
 
 //static folder
 app.use(express.static(path.join(__dirname, 'public')));
