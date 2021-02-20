ad3081219b96a7f14b19e59ac330b307a99b26d7
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-02-20 11:41:43 +0530

    added google authentication strategy and added authentication routes

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const passport = require('passport');
+const session = require('express-session');
 const connectDB = require('./config/db');
 
 // Load config
 dotenv.config({ path: './config/config.env' });
+
+// Passport Config
+require('./config/passport')(passport);
 
 connectDB();
 
@@ -20,11 +25,25 @@ if (process.env.NODE_ENV === 'development') {
 app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
 
+// Sessions should be above passport middleware
+app.use(
+  session({
+    secret: 'a',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 //static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Routes
 app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 3000;
 
