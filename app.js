c5cf2d56c2fd6637a02f6e840195257e6346498e
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-02-20 10:30:16 +0530

    setup template engine

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
 const connectDB = require('./config/db');
 
 // Load config
@@ -8,6 +10,14 @@ dotenv.config({ path: './config/config.env' });
 connectDB();
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.unsubscribe(morgan('dev'));
+}
+
+//Handlebars
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
 
 const PORT = process.env.PORT || 3000;
 
